492d778b42ad27ef82067bd317edd105efdea8cb
LandmarkTechnology <70292434+legah2045@users.noreply.github.com>
2020-11-07 09:52:25 -0500

    Update HelloWorld.java

--- src/main/java/com/mt/sample/HelloWorld.java
+++ src/main/java/com/mt/sample/HelloWorld.java
@@ -9,12 +9,12 @@ import java.util.Date;
 public class HelloWorld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello Welcome to Maven Build Tool !! Today Date is:  " + getLocalCurrentDate());
+		System.out.println("Hello Welcome to Maven Build Tool and LandmarkTechnology !! Today Date is:  " + getLocalCurrentDate());
 
 	}
 
 	private static Date getLocalCurrentDate() {
-		String m = "Mithun Technologies";
+		String m = "Landmark Technologies";
 		return new Date();
 
 	}
